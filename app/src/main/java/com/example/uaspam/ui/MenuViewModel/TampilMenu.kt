f3da0b247cdc2337fd35fa7b879f60fb94c8854e
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:55:32 +0700

    membuat function MenuScreen

--- app/src/main/java/com/example/uaspam/ui/MenuViewModel/TampilMenu.kt
+++ app/src/main/java/com/example/uaspam/ui/MenuViewModel/TampilMenu.kt
@@ -5,24 +5,87 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uaspam.Model.Makanan
+import com.example.uaspam.Navigation.DestinasiNavigasi
+import com.example.uaspam.ui.AddMenuTopAppBar
+import com.example.uaspam.ui.PenyediaViewModel
+
+object TampilScreenMenu : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Kontak"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onPaymentClick: (String) -> Unit = {},
+    onDetailClick: (String) -> Unit = {},
+    viewModel: MenuViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            AddMenuTopAppBar(
+                title = "Makanin",
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = ""
+                )
+            }
+        },
+    ) { innerPadding ->
+        val uiStateMakan by viewModel.homeUIState.collectAsState()
+        BodyHome(
+            itemMakanan = uiStateMakan.listMakanan,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            onViewClick = onDetailClick,
+            onOrderClick = onPaymentClick
+        )
+    }
+}
 
 @Composable
 fun BodyHome(
