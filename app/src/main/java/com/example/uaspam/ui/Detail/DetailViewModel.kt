25f150fcf56083bf31fc6bcee43fbea01a0ea21f
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:23:52 +0700

    membuat class DetailViewModel.kt

--- app/src/main/java/com/example/uaspam/ui/Detail/DetailViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/Detail/DetailViewModel.kt
@@ -1,2 +1,15 @@
 package com.example.uaspam.ui.Detail
 
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.uaspam.data.MakananRepository
+
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repository: MakananRepository
+) : ViewModel() {
+
+
+}
