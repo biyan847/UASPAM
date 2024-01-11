515e6518e51f006e2d383fafaf45683c7a9371c8
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:35:20 +0700

    membuat class MenuViewModel.kt

--- app/src/main/java/com/example/uaspam/ui/MenuViewModel/MenuViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/MenuViewModel/MenuViewModel.kt
@@ -1,10 +1,37 @@
 package com.example.uaspam.ui.MenuViewModel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.Model.Makanan
+import com.example.uaspam.data.MakananRepository
+import com.example.uaspam.ui.MakananUIState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-sealed class MakananUIState {
-    data class Success(val makanan: Flow<List<Makanan>>) : MakananUIState()
-    object Error : MakananUIState()
-    object Loading : MakananUIState()
+sealed class KontakUIState {
+    data class Success(val makanan: Flow<List<Makanan>>) : KontakUIState()
+    object Error : KontakUIState()
+    object Loading : KontakUIState()
+}
+
+class MenuViewModel(private val makananRepository: MakananRepository) : ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUIState: StateFlow<MakananUIState> = makananRepository.getAll()
+        .filterNotNull()
+        .map {
+            MakananUIState(listMakanan = it.toList(), it.size)
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = MakananUIState()
+        )
 }
