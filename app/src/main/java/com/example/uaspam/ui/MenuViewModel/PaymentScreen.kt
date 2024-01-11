66cf8868fa2aaf4b022ee82a11e1d669b3216a12
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:57:11 +0700

    menambahkan private function ItemDetailsRow

--- app/src/main/java/com/example/uaspam/ui/MenuViewModel/PaymentScreen.kt
+++ app/src/main/java/com/example/uaspam/ui/MenuViewModel/PaymentScreen.kt
@@ -1,2 +1,19 @@
 package com.example.uaspam.ui.MenuViewModel
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+private fun ItemDetailsRow(
+    labelResID: String, itemDetail: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = labelResID, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
+}
