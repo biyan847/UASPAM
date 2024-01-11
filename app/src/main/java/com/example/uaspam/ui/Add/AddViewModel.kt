4ab17cdb4f4798d6158c394b9bc257f6014a343d
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:20:10 +0700

    menambahkan file AddViewModel

---
+++ app/src/main/java/com/example/uaspam/ui/Add/AddViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.uaspam.ui.Add
+
+
+import androidx.lifecycle.ViewModel
+import com.example.uaspam.data.MakananRepository
+
+class AddViewModel(private val makananRepository: MakananRepository) : ViewModel() {
+
+
+}
