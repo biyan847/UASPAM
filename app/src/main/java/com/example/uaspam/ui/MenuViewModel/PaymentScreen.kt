76be6dec7c7994a4d823fc62b40fae2b1e8edbd0
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:59:43 +0700

    menambahkan object dan function PaymentScreen

--- app/src/main/java/com/example/uaspam/ui/MenuViewModel/PaymentScreen.kt
+++ app/src/main/java/com/example/uaspam/ui/MenuViewModel/PaymentScreen.kt
@@ -6,18 +6,62 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uaspam.Model.Makanan
+import com.example.uaspam.Navigation.DestinasiNavigasi
+import com.example.uaspam.ui.AddMenuTopAppBar
+import com.example.uaspam.ui.Detail.DetailViewModel
 import com.example.uaspam.ui.DetailUIState
+import com.example.uaspam.ui.PenyediaViewModel
 import com.example.uaspam.ui.toAdmin
+
+object DetailPaymentScreen : DestinasiNavigasi {
+    override val route = "item_payment"
+    override val titleRes = "Detail Pesanan"
+    const val MakananId = "itemId"
+    val routeWithArgs = "$route/{$MakananId}"
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PaymentScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState.collectAsState()
+    Scaffold(
+        topBar = {
+            AddMenuTopAppBar(
+                title = DetailPaymentScreen.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        }, modifier = modifier
+    ) { innerPadding ->
+        ItemDetailsBody(
+            detailUIState = uiState.value,
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()),
+        )
+    }
+}
 
 @Composable
 private fun ItemDetailsBody(
