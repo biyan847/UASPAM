a692d8ef73d3a0466b97b42d1bc28d889a26db11
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:32:05 +0700

    membuat sealed class MakananUIState

--- app/src/main/java/com/example/uaspam/ui/MenuViewModel/MenuViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/MenuViewModel/MenuViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.uaspam.ui.MenuViewModel
 
+import com.example.uaspam.Model.Makanan
+import kotlinx.coroutines.flow.Flow
+
+sealed class MakananUIState {
+    data class Success(val makanan: Flow<List<Makanan>>) : MakananUIState()
+    object Error : MakananUIState()
+    object Loading : MakananUIState()
+}
