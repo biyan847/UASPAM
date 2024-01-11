b41182cb968d79b47b67dbadd43b26c62949d483
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:58:32 +0700

    menambahkan fun ItemDetailsBody

--- app/src/main/java/com/example/uaspam/ui/MenuViewModel/PaymentScreen.kt
+++ app/src/main/java/com/example/uaspam/ui/MenuViewModel/PaymentScreen.kt
@@ -9,13 +9,37 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.uaspam.Model.Makanan
+import com.example.uaspam.ui.DetailUIState
+import com.example.uaspam.ui.toAdmin
 
+@Composable
+private fun ItemDetailsBody(
+    detailUIState: DetailUIState,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        ItemDetails(
+            makanan  = detailUIState.addEvent.toAdmin(), modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedButton(
+            onClick = { },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Check Out")
+        }
+    }
+}
 
 @Composable
 fun ItemDetails(
