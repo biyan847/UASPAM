14a6f8501253b860063f6a54498450929676955b
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:25:51 +0700

    menambahkan val pada class DetailViewModel.kt

--- app/src/main/java/com/example/uaspam/ui/Detail/DetailViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/Detail/DetailViewModel.kt
@@ -3,7 +3,15 @@ package com.example.uaspam.ui.Detail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.data.MakananRepository
+import com.example.uaspam.ui.DetailUIState
+import com.example.uaspam.ui.toDetailAdmin
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 
 class DetailViewModel(
@@ -11,5 +19,25 @@ class DetailViewModel(
     private val repository: MakananRepository
 ) : ViewModel() {
 
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val makananId: String = checkNotNull(savedStateHandle[DetailDestinationScreen.MakananId])
+
+    val uiState: StateFlow<DetailUIState> =
+        repository.getMakananById(makananId)
+            .filterNotNull()
+            .map {
+                DetailUIState(addEvent = it.toDetailAdmin())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailUIState()
+            )
+
+    suspend fun deleteadmin() {
+        repository.delete(makananId)
+    }
 
 }
