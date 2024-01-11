39fdd55f95242c941d8a67c77a035e31bf93657d
febyan <febyan.putra.ft21@mail.umy.ac.id>
2024-01-12 02:11:22 +0700

    datapelanggan

--- app/src/main/java/com/example/uaspam/ui/DataPelanggan/Datapel.kt
+++ app/src/main/java/com/example/uaspam/ui/DataPelanggan/Datapel.kt
@@ -1,7 +1,9 @@
 package com.example.uaspam.ui.DataPelanggan
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -9,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,19 +21,124 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uaspam.Navigation.DestinasiNavigasi
+import com.example.uaspam.R
 import com.example.uaspam.ui.AddPelanggan
+import com.example.uaspam.ui.AddUIState
+import kotlinx.coroutines.launch
 
 
 object DestinasiDataPel : DestinasiNavigasi {
     override val route = "Item_entry"
     override val titleRes = "add pelanggan"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DataPel(
+    navigateNext: () -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    detailDatapelViewModel: DetailDatapelViewModel = viewModel(factory = PenyediaViewModel.comsumen)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            AddMenuTopAppBar(
+                title = DestinasiDataPel.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack )
+        }
+    ) { innerPadding ->
+        Entri(
+            addUIState = detailDatapelViewModel.AddUiState,
+            onDataValueChange = detailDatapelViewModel::UpdateAddUIState,
+            onSaveClick = { coroutineScope.launch {
+                detailDatapelViewModel.addpelanggan()
+            }
+            },
+            navigateNext = {
+                navigateNext()
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+
+    }
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(fraction = 0.3f),
+            Alignment.TopEnd,){
+            Image(painter = painterResource(id = R.drawable.purpleback),
+                contentDescription = "Back App",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds)
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ){
+                Image(painter = painterResource(id = R.drawable.sendok), contentDescription = "Logo App", modifier = Modifier
+                    .weight(5f)
+                    .size(250.dp)
+                )
+            }
+        }
+    }
+
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Entri(
+    addUIState: AddUIState,
+    onDataValueChange: (AddPelanggan) -> Unit,
+    modifier: Modifier = Modifier,
+    onSaveClick: () -> Unit,
+    navigateNext: () -> Unit,
+    detailDatapelViewModel: DetailDatapelViewModel = viewModel(factory = PenyediaViewModel.comsumen)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Column(modifier = modifier.padding(20.dp))
+    {
+        UiInput(
+            addPelanggan = addUIState.addPelanggan,
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = onDataValueChange,
+            onSaveClick = {
+                coroutineScope.launch {
+                    detailDatapelViewModel.addpelanggan()
+                }
+            },
+            OnNextClick = {
+                navigateNext()
+            },
+        )
+    }
 }
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
