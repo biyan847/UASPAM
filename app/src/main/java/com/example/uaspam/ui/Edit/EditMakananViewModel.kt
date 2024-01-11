10c29e662442b6d09c826a344fe44a7c82acefb2
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:28:50 +0700

    menambahkan function didalam class

--- app/src/main/java/com/example/uaspam/ui/Edit/EditMakananViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/Edit/EditMakananViewModel.kt
@@ -1,4 +1,43 @@
 package com.example.uaspam.ui.Edit
 
-class EditMakananViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uaspam.data.MakananRepository
+import com.example.uaspam.ui.AddEvent
+import com.example.uaspam.ui.AddUIState
+import com.example.uaspam.ui.toAdmin
+import com.example.uaspam.ui.toUIStateAdmin
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditMakananViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val makananrepo : MakananRepository
+): ViewModel() {
+    var makanUiState by mutableStateOf(AddUIState())
+        private set
+
+    private  val makananId: String = checkNotNull(savedStateHandle[EditMakananScreen.makananId])
+
+    init {
+        viewModelScope.launch {
+            makanUiState =
+                makananrepo.getMakananById(makananId)
+                    .filterNotNull()
+                    .first()
+                    .toUIStateAdmin()
+        }
+    }
+
+    fun updateUiState(addEvent: AddEvent){
+        makanUiState = makanUiState.copy(addEvent = addEvent)
+    }
+    suspend fun updatemakanan(){
+        makananrepo.update(makanUiState.addEvent.toAdmin())
+    }
 }
