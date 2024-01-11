e52eef8b84001d7f284c0a13c71d6863e0bcaa29
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:57:54 +0700

    menambahkan function ItemDetails

--- app/src/main/java/com/example/uaspam/ui/MenuViewModel/PaymentScreen.kt
+++ app/src/main/java/com/example/uaspam/ui/MenuViewModel/PaymentScreen.kt
@@ -1,11 +1,64 @@
 package com.example.uaspam.ui.MenuViewModel
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.uaspam.Model.Makanan
+
+
+@Composable
+fun ItemDetails(
+    makanan: Makanan, modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            ItemDetailsRow(
+                labelResID ="Nama",
+                itemDetail = makanan.namamkn,
+                modifier = Modifier.padding(
+                    horizontal = 12.dp
+                )
+            )
+            ItemDetailsRow(
+                labelResID = "Harga",
+                itemDetail = makanan.harga,
+                modifier = Modifier.padding(
+                    horizontal = 12.dp
+                )
+            )
+            ItemDetailsRow(
+                labelResID = "Jenis",
+                itemDetail = makanan.jenis,
+                modifier = Modifier.padding(
+                    horizontal = 12.dp
+                )
+            )
+        }
+
+    }
+}
+
 
 @Composable
 private fun ItemDetailsRow(
