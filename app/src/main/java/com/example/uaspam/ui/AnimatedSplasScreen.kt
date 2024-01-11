0f996a9503f2837a0aaeb58b367dcfa062d36ffc
febyan <febyan.putra.ft21@mail.umy.ac.id>
2024-01-12 01:13:49 +0700

    Menambahkan animasi splash screen

--- app/src/main/java/com/example/uaspam/ui/AnimatedSplasScreen.kt
+++ app/src/main/java/com/example/uaspam/ui/AnimatedSplasScreen.kt
@@ -1,18 +1,45 @@
 package com.example.uaspam.ui
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavHostController
 import com.example.uaspam.R
 import com.example.uaspam.ui.theme.Purple80
+import kotlinx.coroutines.delay
 
+@Composable
+fun AnimatedSplashScreen(navController: NavHostController) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 3000
+        )
+    )
+
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+        delay(4000)
+        navController.popBackStack()
+        navController.navigate("LoginPage")
+    }
+    Splash(alpha = alphaAnim.value)
+}
 
 @Composable
 fun Splash(alpha: Float) {
