48aadc341be6cb8088a856437360c06d55a3a99f
febyan <febyan.putra.ft21@mail.umy.ac.id>
2024-01-12 01:58:07 +0700

    Menambahkan menambahkan objek destinasi pelanggan

---
+++ app/src/main/java/com/example/uaspam/ui/DataPelanggan/Datapel.kt
@@ -0,0 +1,10 @@
+package com.example.uaspam.ui.DataPelanggan
+
+import com.example.uaspam.Navigation.DestinasiNavigasi
+
+
+object DestinasiDataPel : DestinasiNavigasi {
+    override val route = "Item_entry"
+    override val titleRes = "add pelanggan"
+}
+
