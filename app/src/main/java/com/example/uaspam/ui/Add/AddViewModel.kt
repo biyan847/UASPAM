f58b01b90690b5d7ce11cd027effebdc4547d6ca
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:21:05 +0700

    menambahkan fun update dan add file AddViewModel

--- app/src/main/java/com/example/uaspam/ui/Add/AddViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/Add/AddViewModel.kt
@@ -1,10 +1,25 @@
 package com.example.uaspam.ui.Add
 
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.uaspam.data.MakananRepository
+import com.example.uaspam.ui.AddEvent
+import com.example.uaspam.ui.AddUIState
+import com.example.uaspam.ui.toAdmin
 
 class AddViewModel(private val makananRepository: MakananRepository) : ViewModel() {
 
+    var addUIState by mutableStateOf(AddUIState())
+        private set
 
+    fun updateAddUIState(addEvent: AddEvent) {
+        addUIState = AddUIState(addEvent = addEvent)
+    }
+
+    suspend fun addMakanan() {
+        makananRepository.save(addUIState.addEvent.toAdmin())
+    }
 }
