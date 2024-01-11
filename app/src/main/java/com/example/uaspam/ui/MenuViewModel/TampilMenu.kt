359eb7db034193a131e1bb10e3df4e1a7fdab12f
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:53:57 +0700

    membuat function ListMakanan

--- app/src/main/java/com/example/uaspam/ui/MenuViewModel/TampilMenu.kt
+++ app/src/main/java/com/example/uaspam/ui/MenuViewModel/TampilMenu.kt
@@ -1,11 +1,14 @@
 package com.example.uaspam.ui.MenuViewModel
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Card
@@ -13,10 +16,35 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.uaspam.Model.Makanan
+
+@Composable
+fun ListMakanan(
+    itemMakanan: List<Makanan>,
+    modifier: Modifier = Modifier,
+    onItemClick: (Makanan) -> Unit,
+    onOrderClick: (Makanan) -> Unit // Tambahkan parameter ini
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        this.items(itemMakanan, key = { it.id }) { makanan ->
+            DataMakan(
+                makanan = makanan,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onItemClick(makanan) }
+            )
+            TextButton(onClick = { onOrderClick(makanan) }) { // Tambahkan fungsi untuk tombol order
+                Text(text = "Order")
+            }
+        }
+    }
+}
 
 @Composable
 fun DataMakan(
