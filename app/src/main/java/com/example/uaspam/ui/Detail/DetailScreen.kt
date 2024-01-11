7957edc55ad307d571e0d33a219c41b69947aaf8
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 03:11:13 +0700

    Membuat DetailScreen.kt dan menambahkan delte

---
+++ app/src/main/java/com/example/uaspam/ui/Detail/DetailScreen.kt
@@ -0,0 +1,27 @@
+package com.example.uaspam.ui.Detail
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do nothing */ },
+        title = { Text("Are you sure") },
+        text = { Text("Delete") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "No")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        })
+}
