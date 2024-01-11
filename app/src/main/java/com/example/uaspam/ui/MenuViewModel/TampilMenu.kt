7e17aa12d51264a0caae63b9ad5c053ca83e7ef8
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-12 02:53:17 +0700

    membuat function DataMakan

--- app/src/main/java/com/example/uaspam/ui/MenuViewModel/TampilMenu.kt
+++ app/src/main/java/com/example/uaspam/ui/MenuViewModel/TampilMenu.kt
@@ -1,2 +1,58 @@
 package com.example.uaspam.ui.MenuViewModel
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.uaspam.Model.Makanan
+
+@Composable
+fun DataMakan(
+    makanan: Makanan,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                Text(
+                    text = makanan.namamkn,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = null,
+                )
+                Text(
+                    text = makanan.harga,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(
+                text = makanan.jenis,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
